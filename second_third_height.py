880f27176c43d1e5ebf073c45251b9f8971ae2c7
khwazaabbas <khwazaabbas1995@gmail.com>
2025-07-22 17:07:47 +0530

    some changes in second third height

--- second_third_height.py
+++ second_third_height.py
@@ -8,5 +8,5 @@ def find_second_third_highest(numbers):
     third_highest = unique_numbers[2]
     print("Second highest number:", second_highest)
     print("Third highest number:", third_highest)
-numbers = [-10, 20, 30, 40, 50, 60, 70, 80, 90, 100, -10, -20, 90, 100]
+numbers = list(map(int,input("enter the list number seperated by the spaces : ").split()))
 find_second_third_highest(numbers)
